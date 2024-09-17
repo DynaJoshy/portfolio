9692d5cf1a1278d0cd300800e11d415610ed3869
DynaJoshy <dynajo11@gmail.com>
2024-09-17 18:45:33 +0530

    Updated

--- script.js
+++ script.js
@@ -23,4 +23,27 @@ function typeWriter() {
 }
 
 typeWriter();
+'use strict';
+
+function progressBarAndCountNumber () {
+    const progress = document.querySelectorAll('.progress');
+    let count = 0;
+    // You must put the maximum number in the MAX variable.
+    let MAX = 80;
+
+    let run = setInterval(() => {
+        count++;
+        progress.forEach(element => {
+            if (count <= element.dataset.progress) {
+                element.parentElement.style.background = `conic-gradient(#f9004d ${count}%, #212428 0)`;
+                element.firstElementChild.textContent = `${count}%`;
+            };
+            if (count == MAX) {
+                clearInterval(run);
+            };
+        });
+    }, 20);
+}
+
+progressBarAndCountNumber();
 
