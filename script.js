2a1f49251294eebf7e4e9419cabbceb7853f668d
DynaJoshy <dynajo11@gmail.com>
2024-10-02 17:29:42 +0530

    updated

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMenu() {
+    const menu =document.querySelector(".menu-links"); 
+    const icon =document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+
+
+
+
+function typeWriter() {
+    if (index < text.length) {
+        textElement.textContent += text.charAt(index);
+        index++;
+        setTimeout(typeWriter, 100); // Adjust the typing speed (milliseconds)
+    }
+}
+
+typeWriter();
+'use strict';
+
+
+
